fa7d7804116603d70e918d97235f9b1cf593f334
Peter Banik <peter@froggle.org>
2017-05-17 01:19:10 +0200

    moved comment

--- createwalletTest.js
+++ createwalletTest.js
@@ -21,6 +21,12 @@ client.createWallet(walletName, copayerName, m, n, opts, function (err, secret) 
 
     console.log('createWallet', err, secret);
 
+    // 4gFHrtUTNz9pqTkicBe7AP2k2M3zLDkpkysZ6ohSi64xCiq9MERWRdt6W12PWgEv6NjWV4V3QjZ7KT
+    //                                                     secret has wrong length ^
+    //
+    // YGs7UXUjncBAxNTLi6DVzCAES8TTzppJMPjx5FGMEkMtFW2TChdQTFizviVEMT4yFtQvThLppxmT
+    // (secret generated on https://wallet.decred.org
+
     try {
 
         client.joinWallet(secret, 'bob', {}, function () {
@@ -29,12 +35,6 @@ client.createWallet(walletName, copayerName, m, n, opts, function (err, secret) 
     }
 
     catch(err){
-        // 4gFHrtUTNz9pqTkicBe7AP2k2M3zLDkpkysZ6ohSi64xCiq9MERWRdt6W12PWgEv6NjWV4V3QjZ7KT
-        //                                                     secret has wrong length ^
-        //
-        // YGs7UXUjncBAxNTLi6DVzCAES8TTzppJMPjx5FGMEkMtFW2TChdQTFizviVEMT4yFtQvThLppxmT
-        // (secret generated on https://wallet.decred.org
-
         console.log(err);
     }
 
